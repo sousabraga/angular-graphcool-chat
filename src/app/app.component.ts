d727d7b38d126ffb4503ecc7534fb489bdc1f93b
Matheus Braga <matheusssbraga@gmail.com>
2019-08-12 14:41:40 +0100

    Implementing simple requests with query and mutation to Graphcool using HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-graphcool-chat';
+
+  private API_URL = 'https://api.graph.cool/simple/v1/cjz2vgfcp672201734p37zaqz';
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.createUser('Ivo Fernandes', 'ivofernandes@gmail.com', '123456');
+    this.allUsers();
+  }
+
+  allUsers(): void {
+    const body = {
+      query: `
+        query {
+          allUsers {
+            id
+            name
+            email
+          }
+        }
+      `
+    };
+
+    this.http.post(this.API_URL, body)
+      .subscribe(response => console.log(`Query: ${JSON.stringify(response)}`));
+  }
+
+  createUser(name: string, email: string, password: string): void {
+    const body = {
+      query: `
+        mutation createNewUser($name: String!, $email: String!, $password: String!) {
+          createUser(name: $name, email: $email, password: $password) {
+            id
+            name
+            email
+          }
+        }
+      `,
+      variables: {
+        name,
+        email,
+        password
+      }
+    };
+
+    this.http.post(this.API_URL, body).subscribe(response => console.log(`Mutation: ${JSON.stringify(response)}`));
+  }
+
 }
