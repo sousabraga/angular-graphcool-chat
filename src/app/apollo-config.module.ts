e1d4ef058a310e58db3d2f9bab5d33fdc22a215e
Matheus Braga <matheusssbraga@gmail.com>
2019-08-12 17:40:09 +0100

    Inserting error handling

--- src/app/apollo-config.module.ts
+++ src/app/apollo-config.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { ApolloModule, Apollo } from 'apollo-angular';
+import { ApolloLink } from 'apollo-link';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
@@ -22,8 +25,20 @@ export class ApolloConfigModule {
       const uri = 'https://api.graph.cool/simple/v1/cjz2vgfcp672201734p37zaqz';
       const http = this.httpLink.create({ uri });
 
+      const linkError = onError(({ graphQLErrors, networkError }) => {
+        if (graphQLErrors) {
+          graphQLErrors.map(({ message, locations, path }) =>
+            console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+          );
+        }
+
+        if (networkError) {
+          console.log(`[Network error]: ${networkError}`);
+        }
+      });
+
       this.apollo.create({
-        link: http,
+        link: ApolloLink.from([linkError, http]),
         cache: new InMemoryCache(),
         connectToDevTools: !environment.production
       });
